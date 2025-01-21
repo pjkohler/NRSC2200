b98c52a7d846fa5d7149ea45bfb9ea0bae658798
pjkohler <pjkohl3r@gmail.com>
2025-01-20 23:35:58 -0500

    use sums instead of rounded means

--- pphys_ebbinghaus/ebbinghaus.py
+++ pphys_ebbinghaus/ebbinghaus.py
@@ -285,8 +285,8 @@ def fit_data(info_df, data_type="same", lightweight=True):
         #    fit_params, fit_options = combine_data(temp_params, fit_params, fit_options)
         temp_params = None 
     # run fit on averages for illustration purposes
-    fit_data = [ int(info_df[x].mean()) for x in info_df.columns if x.startswith("testbigger-{0}-".format(data_type)) ]
-    n_trials = [ int(info_df[x].mean()) for x in info_df.columns if x.startswith("ntrials-{0}-".format(data_type)) ]
+    fit_data = [ info_df[x].sum() for x in info_df.columns if x.startswith("testbigger-{0}-".format(data_type)) ]
+    n_trials = [ info_df[x].sum() for x in info_df.columns if x.startswith("ntrials-{0}-".format(data_type)) ]
     temp_params, threshold, slope = fit_ps(test_inner_sizes, fit_data, n_trials)
     pse_slope.append((threshold, slope))
     #fit_params, fit_options = combine_data(temp_params, fit_params, fit_options)
